38d7d8f77fa66adfc54d44b09e310c46a5b33f57
tapris06 <a01071545040@gmail.com>
2024-01-23 18:06:35 +0900

    포톤 연결

---
+++ Assets/RoomManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class RoomManager : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Connecting...");
+
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        base.OnConnectedToMaster();
+        Debug.Log("connected to Server");
+        PhotonNetwork.JoinLobby();
+    }
+}
