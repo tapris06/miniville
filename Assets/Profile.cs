40c34f49bc14b4148424b2970783283465fd4971
tapris06 <a01071545040@gmail.com>
2024-03-10 18:42:47 +0900

    rescale

---
+++ Assets/Profile.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Profile : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void SetName(string name){
+
+    }
+    void OnSp(int i){
+        
+    }
+    void OffSp(int i){
+
+    }
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
