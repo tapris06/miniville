33f66d197d4a619e7748702ae7d758f2298ebe70
tapris06 <a01071545040@gmail.com>
2024-03-17 18:43:46 +0900

    profile

--- Assets/CardManager.cs
+++ Assets/CardManager.cs
@@ -14,26 +14,32 @@ public class CardManager : MonoBehaviour
         return r;
     }
     
+    
+
+    public Sprite img1;
+
     void Start()
     {
         for(int i = 0; i < 4; i++){
-            CardData.Add(new Tuple<string,int,int,int,int,Sprite>("밀밭",1,1,1,1,null));
-            CardData.Add(new Tuple<string,int,int,int,int,Sprite>("목장",1,2,2,1,null));
-            CardData.Add(new Tuple<string,int,int,int,int,Sprite>("빵집",2,2,3,1,null));
-            CardData.Add(new Tuple<string,int,int,int,int,Sprite>("카페",3,3,3,2,null));
-            CardData.Add(new Tuple<string,int,int,int,int,Sprite>("편의점",2,4,4,2,null));
-            CardData.Add(new Tuple<string,int,int,int,int,Sprite>("숲",1,5,5,3,null));
-            CardData.Add(new Tuple<string,int,int,int,int,Sprite>("경기장",4,6,6,6,null));
-            CardData.Add(new Tuple<string,int,int,int,int,Sprite>("전시장",4,6,6,8,null));
-            CardData.Add(new Tuple<string,int,int,int,int,Sprite>("TV방송국",4,6,6,7,null));
-            CardData.Add(new Tuple<string,int,int,int,int,Sprite>("치즈공장",2,1,1,5,null));
-            CardData.Add(new Tuple<string,int,int,int,int,Sprite>("가구공장",2,1,1,3,null));
-            CardData.Add(new Tuple<string,int,int,int,int,Sprite>("광산",1,1,1,6,null));
-            CardData.Add(new Tuple<string,int,int,int,int,Sprite>("패밀리레스토랑",3,1,1,3,null));
-            CardData.Add(new Tuple<string,int,int,int,int,Sprite>("사과밭",1,1,1,3,null));
-            CardData.Add(new Tuple<string,int,int,int,int,Sprite>("농산물시장",2,1,1,2,null));
+            CardData.Add(new Tuple<string,int,int,int,int,Sprite>("밀밭",1,1,1,1,img1));
+            CardData.Add(new Tuple<string,int,int,int,int,Sprite>("목장",1,2,2,1,img1));
+            CardData.Add(new Tuple<string,int,int,int,int,Sprite>("빵집",2,2,3,1,img1));
+            CardData.Add(new Tuple<string,int,int,int,int,Sprite>("카페",3,3,3,2,img1));
+            CardData.Add(new Tuple<string,int,int,int,int,Sprite>("편의점",2,4,4,2,img1));
+            CardData.Add(new Tuple<string,int,int,int,int,Sprite>("숲",1,5,5,3,img1));
+            CardData.Add(new Tuple<string,int,int,int,int,Sprite>("경기장",4,6,6,6,img1));
+            CardData.Add(new Tuple<string,int,int,int,int,Sprite>("전시장",4,6,6,8,img1));
+            CardData.Add(new Tuple<string,int,int,int,int,Sprite>("TV방송국",4,6,6,7,img1));
+            CardData.Add(new Tuple<string,int,int,int,int,Sprite>("치즈공장",2,1,1,5,img1));
+            CardData.Add(new Tuple<string,int,int,int,int,Sprite>("가구공장",2,1,1,3,img1));
+            CardData.Add(new Tuple<string,int,int,int,int,Sprite>("광산",1,1,1,6,img1));
+            CardData.Add(new Tuple<string,int,int,int,int,Sprite>("패밀리레스토랑",3,1,1,3,img1));
+            CardData.Add(new Tuple<string,int,int,int,int,Sprite>("사과밭",1,1,1,3,img1));
+            CardData.Add(new Tuple<string,int,int,int,int,Sprite>("농산물시장",2,1,1,2,img1));
         }
-        Debug.Log(Draw());
+        Tuple<string,int,int,int,int,Sprite> tmp = Draw();
+        Debug.Log(tmp);
+        
     }
 
     void Update()
--- Assets/Profile.cs
+++ Assets/Profile.cs
@@ -5,19 +5,33 @@ using UnityEngine.UI;
 
 public class Profile : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Image mark1;
+    public Image mark2;
+    public Image mark3;
+    public Image mark4;
+    public Text Name;
+    public Sprite OnState;
+    public Sprite OffState;
+    public Text Coin;
+    int coin;
     void SetName(string name){
 
     }
     void OnSp(int i){
-        
+        if(i==1)mark1.sprite=OnState;
+        else if(i==2)mark2.sprite=OnState;
+        else if(i==3)mark3.sprite=OnState;
+        else mark4.sprite=OnState;
     }
     void OffSp(int i){
-
+        if(i==1)mark1.sprite=OffState;
+        else if(i==2)mark2.sprite=OffState;
+        else if(i==3)mark3.sprite=OffState;
+        else mark4.sprite=OffState;
     }
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
